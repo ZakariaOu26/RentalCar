1f6ee90d8c715bd9b3f3a1d6bebeac654748c3a8
Zakaria Oubaios <the.best.messi2016@gmail.com>
2026-03-02 22:50:26 +0000

    Add Python script: scripts/script_20260302_225024.py

---
+++ scripts/script_20260302_225024.py
@@ -0,0 +1,9 @@
+# Auto-generated script
+# Created: 2026-03-02T22:50:24.835780
+# Account: Account 1
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+if __name__ == "__main__":
+    print("Script executed successfully!")
